648e4e23c998c64b67e5b0b5c3447d58b1ad5acb
arafatfaisal <arafatfaisal98@gmail.com>
2024-11-10 18:57:37 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="DSSS-homework",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here if needed
+)
